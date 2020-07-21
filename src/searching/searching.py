77ee1fd58fb982ed74b3e6762d1e005be7b4e9ed
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-21 13:00:46 -0500

    Recursive implementation of binary search MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,18 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
-
+    # Let's first ensure the start is greater than the end.
+    if start > end:
+        return -1  # If it's not, we're supposed to return -1 (not found)
+    middle = (start + end) // 2  # Calculate the middle.
+    # If we are at the target, go ahead and return the 'middle'
+    if target == arr[middle]:
+        return middle
+    # If the target is less than the middle array index, take 1 off the middle.
+    elif target < arr[middle]:
+        return binary_search(arr, target, start, middle-1)
+    # If the target is greater than the middle array index, add 1 to the middle.
+    elif target > arr[middle]:
+        return binary_search(arr, target, middle+1, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +22,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
